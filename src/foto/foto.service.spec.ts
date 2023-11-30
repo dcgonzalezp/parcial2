e037cc9beed07da2178c8a934e126ae62e855a2d
dcgonzalezp <88750590+dcgonzalezp@users.noreply.github.com>
2023-11-30 10:48:58 -0500

    commit final

--- src/foto/foto.service.spec.ts
+++ src/foto/foto.service.spec.ts
@@ -1,8 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FotoService } from './foto.service';
+import { Repository } from 'typeorm';
+import { FotoEntity } from './foto.entity/foto.entity';
 
 describe('FotoService', () => {
   let service: FotoService;
+  let repository: Repository<FotoEntity>;
+  let fotolist: FotoEntity[];
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
+      providers: [AlbumService],
+    }).compile();
+
+    service = module.get<AlbumService>(AlbumService);
+    repository = module.get<Repository<AlbumEntity>>(getRepositoryToken(AlbumEntity));
+    await seedDatabase();
+  });
+
+
+  const seedDatabase = async () => {
+    repository.clear();
+    albumlist = [];
+    for(let i = 0; i < 5; i++){
+        const album: AlbumEntity = await repository.save({
+            nombre: faker.lorem.word(),
+            fecha: faker.date.past(),
+            caratula: faker.lorem.word(),
+            descripcion: faker.lorem.word()})
+        albumlist.push(album);
+    }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
--- src/shared/testing-utils/typeorm-testing-config.ts
+++ src/shared/testing-utils/typeorm-testing-config.ts
@@ -1,36 +1,22 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ArtistEntity } from '../../artist/artist.entity/artist.entity';
-import { ArtworkEntity } from '../../artwork/artwork.entity/artwork.entity';
-import { ExhibitionEntity } from '../../exhibition/exhibition.entity/exhibition.entity';
-import { ImageEntity } from '../../image/image.entity/image.entity';
-import { MovementEntity } from '../../movement/movement.entity/movement.entity';
-import { MuseumEntity } from '../../museum/museum.entity/museum.entity';
-import { SponsorEntity } from '../../sponsor/sponsor.entity/sponsor.entity';
+import { AlbumEntity } from 'src/album/album.entity/album.entity';
+import { FotoEntity } from 'src/foto/foto.entity/foto.entity';
+import { RedsocialEntity } from 'src/redsocial/redsocial.entity/redsocial.entity';
+import { UsuarioEntity } from 'src/usuario/usuario.entity/usuario.entity';
 
 export const TypeOrmTestingConfig = () => [
   TypeOrmModule.forRoot({
     type: 'sqlite',
     database: ':memory:',
     dropSchema: true,
-    entities: [
-      ArtistEntity,
-      ArtworkEntity,
-      ExhibitionEntity,
-      ImageEntity,
-      MovementEntity,
-      MuseumEntity,
-      SponsorEntity,
-    ],
+    entities: [AlbumEntity, FotoEntity, RedsocialEntity, UsuarioEntity],
     synchronize: true,
     keepConnectionAlive: true,
   }),
   TypeOrmModule.forFeature([
-    ArtistEntity,
-    ArtworkEntity,
-    ExhibitionEntity,
-    ImageEntity,
-    MovementEntity,
-    MuseumEntity,
-    SponsorEntity,
+    AlbumEntity,
+    FotoEntity,
+    RedsocialEntity,
+    UsuarioEntity,
   ]),
 ];
